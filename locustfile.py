c401709cbf1389e5f66694fc6965f0d26beff50c
Yugendran-Sunder <yugendran.sunder@gmail.com>
2025-03-04 23:24:13 +0530

    files added

---
+++ locustfile.py
@@ -0,0 +1,45 @@
+from locust import HttpUser, task
+import random
+import requests
+
+# users=[]
+# for i in requests.get("http://localhost:8080/api/user",
+#                       headers={"Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWdubyI6MTI1MTU2MTQ0LCJpYXQiOjE3MTYzOTcxOTR9.CQksCo7SM9iz9svQQEMHkknHoQTfuo5dp4QPWpptjV4"},
+#                         ).json()['data']:
+#     users.append(i['regno'])
+
+class MyUser(HttpUser):
+    @task
+    def createTicket(self):
+        regno = random.randint(100000000, 100005000)
+        phone = random.randint(6000000000, 9999999999)
+
+        self.client.post(url="/api/user",
+                        json={
+                            "regno":regno, 
+                            "name": "Yugendran", 
+                            "mobile":phone,
+                            "email":str(regno)+"@sastra.ac.in",
+                            "department":"CSE", 
+                            "year":1, 
+                            "ishosteller": True, 
+                            "gender":"Male",
+                            "role":"USER"
+                        }
+        )
+        # regno = random.choice(users)
+        # regno = random.randint(100000000, 100005000)
+        self.client.post(url="/api/ticket",
+                        headers={"Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWdubyI6MTI1MTU2MTQ0LCJpYXQiOjE3MTYzOTcxOTR9.CQksCo7SM9iz9svQQEMHkknHoQTfuo5dp4QPWpptjV4"},
+                        json={
+                            "userId": regno, 
+                            "eventId":1,
+                            "answerList":[
+                                {
+                                    "answer":"hi da",
+                                    "questionId":1
+                                }
+                            ]
+                        }
+                        )
+        
